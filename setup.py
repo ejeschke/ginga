037fd00f9b76f086f84620fbd8ba919548d177a2
Eric Jeschke <eric@naoj.org>
2013-04-10 09:49:37 -1000

    Fixes to distutils script

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 #! /usr/bin/env python
 #
 from distutils.core import setup
-import version
+from ginga.version import version
 
 setup(
     name = "Ginga",
-    version = version.version,
+    version = version,
     author = "Eric Jeschke",
     author_email = "eric@naoj.org",
     description = ("An astronomical (FITS) image viewer."),
     license = "BSD",
     keywords = "FITS image viewer astronomy",
     url = "http://ejeschke.github.com/ginga",
-    package_dir = { '': 'Ginga' },
     packages = ['ginga', 'ginga.gtkw', 'ginga.gtkw.plugins', 'ginga.gtkw.tests',
                 'ginga.qtw', 'ginga.qtw.plugins', 'ginga.qtw.tests',
                 'ginga.misc', 'ginga.misc.plugins',
-                'ginga.icons', 'ginga.tests', 'ginga.util',
+                'ginga.icons', 'ginga.util',
                 'ginga.doc'],
     package_data = { 'icons': ['*.ppm', '*.png'], 'doc': ['manual/*.html'], },
     scripts = ['ginga.py'],
