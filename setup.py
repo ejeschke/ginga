7d12a7b1b109610bc0bb42e73cf549a09d07dc99
Eric Jeschke <eric@naoj.org>
2013-04-15 14:10:50 -1000

    Fixed setup.py script to include license file
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@
 #
 from distutils.core import setup
 from ginga.version import version
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "Ginga",
     version = version,
     author = "Eric Jeschke",
     author_email = "eric@naoj.org",
-    description = ("An astronomical (FITS) image viewer."),
+    description = ("An astronomical (FITS) image viewer and toolkit."),
+    long_description = read('README.txt'),
     license = "BSD",
     keywords = "FITS image viewer astronomy",
     url = "http://ejeschke.github.com/ginga",
@@ -19,8 +24,9 @@ setup(
                 'ginga.doc'],
     package_data = { 'ginga.icons': ['*.ppm', '*.png'],
                      'ginga.doc': ['manual/*.html'],
-                     'ginga.gtkw': ['gtk_rc']
+                     'ginga.gtkw': ['gtk_rc'],
                      },
+    data_files = [('', ['LICENSE.txt', 'README.txt'])],
     scripts = ['scripts/ginga'],
     classifiers = [
         "Development Status :: 5 - Stable",
