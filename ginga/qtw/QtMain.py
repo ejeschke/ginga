138632ac353f6577aac607c80cb54723dcbc288c
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-04-16 22:48:58 +0200

    Fix importing of Queue module

--- ginga/qtw/QtMain.py
+++ ginga/qtw/QtMain.py
@@ -36,7 +36,7 @@ Usage:
 import sys, traceback
 import thread, threading
 import logging
-from Queue import Queue
+import Queue as qu
 
 from ginga.qtw.QtHelp import QtGui, QtCore
 from ginga.misc import Task, Future
@@ -46,7 +46,7 @@ class QtMain(object):
     def __init__(self, queue=None, logger=None, ev_quit=None):
         # You can pass in a queue if you prefer to do so
         if not queue:
-            queue = Queue()
+            queue = qu.Queue()
         self.gui_queue = queue
         # You can pass in a logger if you prefer to do so
         if not logger:
@@ -98,7 +98,7 @@ class QtMain(object):
                     pass
 
                     
-            except Queue.Empty:
+            except qu.Empty:
                 done = True
                 
             except Exception, e:
