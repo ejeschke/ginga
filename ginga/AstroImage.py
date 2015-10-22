ejeschke/ginga

commit 593c2fc50ca47b96b4020ec8a9fe108725560ef6

Added loading HDU by (hduname, extver)
- fix for github.com issue #175
- user can tack on the filename to load any of the following:
    [num], [name], [name,ver]

ginga/AstroImage.py
@@ -16,7 +16,7 @@ import traceback
 
 import numpy, numpy.ma
 
-from ginga.util import wcsmod, io_fits
+from ginga.util import wcsmod, io_fits, iohelper
 from ginga.util import wcs, iqcalc
 from ginga.BaseImage import BaseImage, ImageError, Header
 from ginga.misc import Bunch
@@ -112,14 +112,10 @@ class AstroImage(BaseImage):
 
         ahdr = self.get_header()
 
-        # User specified an HDU using bracket notation at end of path?
-        match = re.match(r'^(.+)\[(\d+)\]$', filepath)
-        if match:
-            filepath = match.group(1)
-            numhdu = max(int(match.group(2)), 0)
+        info = iohelper.get_fileinfo(filepath)
 
-        _data, numhdu, naxispath = self.io.load_file(filepath, ahdr,
-                                                     numhdu=numhdu,
+        _data, numhdu, naxispath = self.io.load_file(info.filepath, ahdr,
+                                                     numhdu=info.numhdu,
                                                      naxispath=naxispath,
                                                      phdr=self._primary_hdr)
         # this is a handle to the full data array
@@ -132,17 +128,11 @@ class AstroImage(BaseImage):
         if len(naxispath) == 0:
             naxispath = ([0] * (len(_data.shape)-2))
 
-        # Set the name to the filename (minus extension) if no name
-        # currently exists for this image
+        # Set the image name if no name currently exists for this image
+        # TODO: should this *change* the existing name, if any
         name = self.get('name', None)
         if name is None:
-            dirpath, filename = os.path.split(filepath)
-            name, ext = os.path.splitext(filename)
-            # Remove trailing .extension
-            if '.' in name:
-                name = name[:name.rindex('.')]
-            if numhdu is not None:
-                name += ('[%d]' % numhdu)
+            name = info.name
             self.set(name=name)
 
         self.set(path=filepath, idx=numhdu)

ginga/Control.py
@@ -537,13 +537,10 @@ class GingaControl(Callback.Callbacks):
         raise ControlError("Can't determine file type of '%s'" % (filepath))
 
     def load_image(self, filepath, idx=None):
-        # User specified an HDU using bracket notation at end of path?
-        match = re.match(r'^(.+)\[(\d+)\]$', filepath)
-        if match:
-            filepfx = match.group(1)
-            idx = max(int(match.group(2)), 0)
-        else:
-            filepfx = filepath
+
+        info = iohelper.get_fileinfo(filepath, cache_dir=self.tmpdir)
+        filepfx = info.filepath
+        idx = info.numhdu
 
         # Create an image.  Assume type to be an AstroImage unless
         # the MIME association says it is something different.
@@ -675,8 +672,7 @@ class GingaControl(Callback.Callbacks):
         kwdargs = {}
         idx = None
         if info.numhdu is not None:
-            idx = max(0, info.numhdu)
-            kwdargs['idx'] = idx
+            kwdargs['idx'] = info.numhdu
 
         try:
             image = image_loader(filepath, **kwdargs)

ginga/util/iohelper.py
@@ -18,13 +18,27 @@ def get_fileinfo(filespec, cache_dir='/tmp', download=False):
     """
     Parse a file specification and return information about it.
     """
-    numhdu = None
+    idx = None
+    name_ext = ''
 
     # User specified an HDU using bracket notation at end of path?
-    match = re.match(r'^(.+)\[(\d+)\]$', filespec)
+    match = re.match(r'^(.+)\[([\w_\-\,\s]+)\]$', filespec)
     if match:
         filespec = match.group(1)
-        numhdu = int(match.group(2))
+        idx = match.group(2)
+        if ',' in idx:
+            hduname, extver = idx.split(',')
+            hduname = hduname.strip()
+            extver = int(extver)
+            idx = (hduname, extver)
+            name_ext = "[%s,%d]" % idx
+        else:
+            if re.match(r'^\d+$', idx):
+                idx = int(idx)
+                name_ext = "[%d]" % idx
+            else:
+                idx = idx.strip()
+                name_ext = "[%s]" % idx
     else:
         filespec = filespec
 
@@ -57,8 +71,12 @@ def get_fileinfo(filespec, cache_dir='/tmp', download=False):
 
     ondisk = os.path.exists(filepath)
 
-    res = Bunch.Bunch(filepath=filepath, url=url, numhdu=numhdu,
-                      ondisk=ondisk)
+    dirname, fname = os.path.split(filepath)
+    fname_pfx, fname_sfx = os.path.splitext(fname)
+    name = fname_pfx + name_ext
+
+    res = Bunch.Bunch(filepath=filepath, url=url, numhdu=idx,
+                      name=name, ondisk=ondisk)
     return res
 
 
@@ -69,5 +87,16 @@ def name_image_from_path(path, idx=None):
     #if '.' in name:
     #    name = name[:name.rindex('.')]
     if idx is not None:
-        name = '%s[%d]' % (name, idx)
+        if isinstance(idx, tuple):
+            assert len(idx) == 2, ValueError("idx tuple len (%d) != 2" % (
+                len(idx)))
+            hduname, extver = idx
+            hduname = hduname.strip()
+            extver = int(extver)
+            name += "[%s,%d]" % idx
+        else:
+            if isinstance(idx, str):
+                name += "[%s]" % idx.strip()
+            else:
+                name += "[%d]" % idx
     return name

commit 3722233a373ddd29ccca862b0220a55609f8879f

Fixed loading multi-ext that broke in 593c2fc

ginga/AstroImage.py
@@ -135,6 +135,8 @@ class AstroImage(BaseImage):
         name = self.get('name', None)
         if name is None:
             name = info.name
+            if ('[' not in name):
+                name += '[{0}]'.format(numhdu)
             self.set(name=name)
 
         self.set(path=filepath, idx=numhdu)

ginga/Control.py
@@ -536,7 +536,7 @@ class GingaControl(Callback.Callbacks):
 
         raise ControlError("Can't determine file type of '%s'" % (filepath))
 
-    def load_image(self, filepath, idx=None):
+    def load_image(self, filepath):
 
         info = iohelper.get_fileinfo(filepath, cache_dir=self.tmpdir)
         filepfx = info.filepath