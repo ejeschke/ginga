2c3d136a356f92c876b91a74d3be9f4b1ab2513e
Eric Jeschke <eric@naoj.org>
2015-11-10 12:07:41 -1000

    Fix for missing ColorBar file in ginga.gw

---
+++ ginga/gw/ColorBar.py
@@ -0,0 +1,9 @@
+# Figure out which widget set we are using and import those canvas types
+from ginga import toolkit
+tkname = toolkit.get_family()
+
+if tkname == 'gtk':
+    from ginga.gtkw.ColorBar import *
+
+elif tkname == 'qt':
+    from ginga.qtw.ColorBar import *
