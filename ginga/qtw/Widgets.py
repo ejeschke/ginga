96caf10191e7b9e3319d3515ada206d579b2649b
Pey Lian Lim <lim@stsci.edu>
2015-11-25 14:06:14 -0500

    Qt TreeView should be sorted by leaf index

--- ginga/qtw/Widgets.py
+++ ginga/qtw/Widgets.py
@@ -567,9 +567,7 @@ class TreeView(WidgetBase):
         treeview = self.widget
         treeview.setColumnCount(len(columns))
         treeview.setSortingEnabled(self.sortable)
-        if self.sortable:
-            # Sort increasing by default
-            treeview.sortByColumn(1, QtCore.Qt.AscendingOrder)
+
         # speeds things up a bit
         treeview.setUniformRowHeights(True)
 
@@ -583,6 +581,10 @@ class TreeView(WidgetBase):
 
         self.datakeys = datakeys
         self.leaf_idx = datakeys.index(self.leaf_key)
+
+        if self.sortable:
+            # Sort increasing by default
+            treeview.sortByColumn(self.leaf_idx, QtCore.Qt.AscendingOrder)
 
         treeview.setHeaderLabels(headers)
 
