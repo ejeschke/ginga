416f5d71c85e7988c2db5971f55b7469de26d9f0
Rajul <rajul09@gmail.com>
2016-01-21 23:09:52 +0700

    Added unit tests for colors.lookup_color() function

--- ginga/tests/test_colors.py
+++ ginga/tests/test_colors.py
@@ -18,6 +18,41 @@ class TestColors(unittest.TestCase):
 		self.logger = logging.getLogger("TestColors")
 		self.initial_color_list_length = len(color_list)
 
+	def test_lookup_color_white_tuple(self):
+		expected = (1.0, 1.0, 1.0)
+		actual = lookup_color("white", "tuple")
+
+		assert np.allclose(expected, actual)
+
+
+	def test_lookup_color_black_tuple(self):
+		expected = (0.0, 0.0, 0.0)
+		actual = lookup_color("black", "tuple")
+
+		assert np.allclose(expected, actual)
+
+	def test_lookup_color_white_hash(self):
+		expected = "#ffffff"
+		actual = lookup_color("white", "hash")
+
+		assert expected == actual
+
+
+	def test_lookup_color_black_black(self):
+		expected = "#000000"
+		actual = lookup_color("black", "hash")
+
+		assert expected == actual
+
+	def test_lookup_color_yellow_tuple(self):
+		expected = (1.0, 1.0, 0.0)
+		actual = lookup_color("yellow")
+
+		assert np.allclose(expected, actual)
+
+	def test_lookup_color_raise_exception(self):
+		self.assertRaises(ValueError, lookup_color, "white", "unknown_format")
+
 	def tearDown(self):
 		pass
 
