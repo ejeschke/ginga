0d1f2c9702697b0d2d606251a5b9dba8395e4e7f
Rajul <rajul09@gmail.com>
2016-01-24 11:24:52 +0700

    Added test_init for Callbacks

--- ginga/misc/tests/test_Callback.py
+++ ginga/misc/tests/test_Callback.py
@@ -7,7 +7,7 @@ import unittest
 import logging
 import numpy as np
 
-import ginga.misc.Callback
+import ginga.misc.Callback as Callback
 
 
 class TestError(Exception):
@@ -18,7 +18,19 @@ class TestCallbacks(unittest.TestCase):
 	def setUp(self):
 		pass
 
-	
+	def test_init(self):
+		test_callbacks = Callback.Callbacks()
+
+		assert isinstance(test_callbacks.cb, dict)
+
+		expected = 0
+		actual = len(test_callbacks.cb)
+		assert expected == actual
+
+		expected = {}
+		actual = test_callbacks.cb
+		assert expected == actual
+		
 	def tearDown(self):
 		pass
 
