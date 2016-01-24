689c0c955d2959d04e73702909be146e20dc61ce
Rajul <rajul09@gmail.com>
2016-01-24 11:17:03 +0700

    Added the TestCallbacks class

---
+++ ginga/misc/tests/test_Callback.py
@@ -0,0 +1,28 @@
+#
+# Unit Tests for the Callbacks class
+#
+# Rajul Srivastava  (rajul09@gmail.com)
+#
+import unittest
+import logging
+import numpy as np
+
+import ginga.misc.Callback
+
+
+class TestError(Exception):
+    pass
+
+
+class TestCallbacks(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	
+	def tearDown(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
+
+#END
