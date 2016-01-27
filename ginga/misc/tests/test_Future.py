02c824bebab696b1491e4aaec8fd34120d8027ff
Rajul <rajul09@gmail.com>
2016-01-27 21:31:47 +0700

    Added the TestFuture class

---
+++ ginga/misc/tests/test_Future.py
@@ -0,0 +1,28 @@
+#
+# Unit Tests for the Future class
+#
+# Rajul Srivastava  (rajul09@gmail.com)
+#
+import unittest
+import logging
+import numpy as np
+
+import ginga.misc.Future as Future
+
+
+class TestError(Exception):
+    pass
+
+
+class TestFuture(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
+# END
