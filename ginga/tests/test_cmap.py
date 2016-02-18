ca0ec6216845671997bd03e8bd4060d3826a7ec1
Rajul <rajul09@gmail.com>
2016-02-18 23:36:07 +0700

    Added test class for cmap file

---
+++ ginga/tests/test_cmap.py
@@ -0,0 +1,18 @@
+#
+# Unit Tests for the colors.py functions
+#
+# Rajul Srivastava  (rajul09@gmail.com)
+#
+import unittest
+import logging
+import numpy as np
+
+import ginga.cmap
+
+
+class TestError(Exception):
+    pass
+
+class TestCmap(unittest.TestCase):
+	pass
+	
