e5a2928a4d8e6e391ce9f4bdfd5fe8e5fb5c8e14
Rajul <rajul09@gmail.com>
2016-02-19 01:06:28 +0700

    Added test for ColorMap intialization

--- ginga/tests/test_cmap.py
+++ ginga/tests/test_cmap.py
@@ -8,11 +8,42 @@ import logging
 import numpy as np
 
 import ginga.cmap
+from ginga.cmap import ColorMap
 
 
 class TestError(Exception):
     pass
 
+
 class TestCmap(unittest.TestCase):
-	pass
-	
+
+    def setUp(self):
+        pass
+
+    def test_ColorMap_init(self):
+        test_clst = ((0.0, 0.0, 0.0), (1.0, 1.0, 1.0))
+        test_color_map = ColorMap('test-name', test_clst)
+
+        expected = 'test-name'
+        actual = test_color_map.name
+        assert expected == actual
+
+        expected = 2
+        actual = len(test_color_map.clst)
+        assert expected == actual
+
+        expected = (0.0, 0.0, 0.0)
+        actual = test_color_map.clst[0]
+        assert np.allclose(expected, actual)
+
+        expected = (1.0, 1.0, 1.0)
+        actual = test_color_map.clst[1]
+        assert np.allclose(expected, actual)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
+# END
