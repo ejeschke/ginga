86db5c178c39aab52cb14a3f65f251ae36630682
Pey Lian Lim <lim@stsci.edu>
2016-04-28 16:12:22 -0400

    Added doc for Mosaic plugin.

--- ginga/misc/plugins/Mosaic.py
+++ ginga/misc/plugins/Mosaic.py
@@ -566,4 +566,11 @@ class Mosaic(GingaPlugin.LocalPlugin):
     def __str__(self):
         return 'mosaic'
 
+
+# Replace module docstring with config doc for auto insert by Sphinx.
+# In the future, if we need the real docstring, we can append instead of
+# overwrite.
+from ginga.util.toolbox import generate_cfg_example
+__doc__ = generate_cfg_example('plugin_Mosaic', package='ginga')
+
 #END
