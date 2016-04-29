0e8cd4dfda424b56cd7fe85fb633154b606f5249
Pey Lian Lim <lim@stsci.edu>
2016-04-29 14:48:59 -0400

    Shorten displayed image name on ChangeHistory more info section to avoid stretching out plugin widget width. Removed unicode_literals from my plugins (was told that is not good idea).

--- ginga/misc/plugins/SaveImage.py
+++ ginga/misc/plugins/SaveImage.py
@@ -1,6 +1,5 @@
 """Save output images local plugin for Ginga."""
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function
 from ginga.util.six import itervalues
 from ginga.util.six.moves import map
 
