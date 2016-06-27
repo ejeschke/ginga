9b4db4a124ab7a3474ebc90ea41dd384683a0d65
Pey Lian Lim <lim@stsci.edu>
2016-06-27 12:56:30 -0400

    ModuleManager: Fixed py3 compat with reload usage. Added docstrings. Minor PEP8 stuff.

--- ginga/misc/ModuleManager.py
+++ ginga/misc/ModuleManager.py
@@ -1,3 +1,4 @@
+"""Dynamically manage module imports."""
 #
 # ModuleManager.py -- Simple class to dynamically manage module imports.
 #
@@ -7,7 +8,15 @@
 # This is open-source software licensed under a BSD license.
 # Please see the file LICENSE.txt for details.
 #
+from __future__ import absolute_import
+
+from ..util.six.moves import reload_module
+
+__all__ = ['ModuleManager']
+
+
 def my_import(name):
+    """Return imported module for the given name."""
     mod = __import__(name)
     components = name.split('.')
     for comp in components[1:]:
@@ -16,20 +25,24 @@ def my_import(name):
 
 
 class ModuleManagerError(Exception):
+    """Exception related to module manager."""
     pass
 
+
 class ModuleManager(object):
+    """Simple class to dynamically manage module imports."""
 
     def __init__(self, logger):
         self.logger = logger
-        
+
         self.module = {}
 
     def loadModule(self, moduleName, pfx=None):
+        """Load module from the given name."""
         try:
             if moduleName in self.module:
                 self.logger.info("Reloading module '%s'..." % moduleName)
-                module = reload(self.module[moduleName])
+                module = reload_module(self.module[moduleName])
 
             else:
                 if pfx:
@@ -39,7 +52,7 @@ class ModuleManager(object):
 
                 self.logger.info("Loading module '%s'..." % moduleName)
                 module = my_import(name)
-                
+
             self.module[moduleName] = module
 
         except Exception as e:
@@ -48,8 +61,7 @@ class ModuleManager(object):
             raise ModuleManagerError(e)
 
     def getModule(self, moduleName):
+        """Return loaded module from the given name."""
         return self.module[moduleName]
 
-    
-            
 #END
