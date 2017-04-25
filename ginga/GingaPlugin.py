ff9216cb4de059a7e2238e7e7e57cf1df4337b6e
Pey Lian Lim <lim@stsci.edu>
2017-04-25 13:53:02 -0400

    Address comments from Eric J.

--- ginga/GingaPlugin.py
+++ ginga/GingaPlugin.py
@@ -6,50 +6,43 @@
 #
 from ginga.misc import Bunch
 
+__all__ = ['PluginError', 'GlobalPlugin', 'LocalPlugin']
+
 
 class PluginError(Exception):
+    """Plugin related error."""
     pass
 
 
-class GlobalPlugin(object):
-
+class BasePlugin(object):
+    """Base class for all plugins."""
     def __init__(self, fv):
-        super(GlobalPlugin, self).__init__()
+        super(BasePlugin, self).__init__()
         self.fv = fv
         self.logger = fv.logger
 
         # Holds GUI widgets
         self.w = Bunch.Bunch()
 
-    def initialize(self, container):
-        """This method will be called with a container widget if the global
-        plugin was requested to be loaded into a workspace.  The plugin should
-        construct its GUI and pack it into the container.
-        """
-        pass
-
     def start(self):
-        """This method is called to start the plugin.  It is called after
-        build_gui().
+        """
+        This method is called to start the plugin.
+        It is called after build_gui().
         """
         pass
 
     def stop(self):
-        """This method is called to stop the plugin.
-        """
+        """This method is called to stop the plugin."""
         pass
 
-    def redo(self, channel, image):
-        """This method is called when an image is set in a channel.
-        """
-        pass
+    def _help_docstring(self):
+        self.fv.help_text(str(self).capitalize(), self.__doc__,
+                          text_kind='rst', trim_pfx=4)
 
     def help(self):
-        name = str(self)
-
+        """Display help for the plugin."""
         if not self.fv.gpmon.has_plugin('WBrowser'):
-            self.fv.help_text(name.capitalize(), self.__doc__, text_kind='rst',
-                              trim_pfx=4)
+            self._help_docstring()
             return
 
         from ginga.doc.download_doc import get_doc
@@ -61,15 +54,34 @@ class GlobalPlugin(object):
 
         obj = self.fv.gpmon.get_plugin('WBrowser')
         url = get_doc(plugin=self)
-        obj.browse(url)
+        if url is None:
+            self._help_docstring()
+        else:
+            obj.browse(url)
 
 
-class LocalPlugin(object):
+class GlobalPlugin(BasePlugin):
+    """Class to handle a global plugin."""
+    def __init__(self, fv):
+        super(GlobalPlugin, self).__init__(fv)
 
+    def initialize(self, container):
+        """
+        This method will be called with a container widget if the global
+        plugin was requested to be loaded into a workspace.  The plugin should
+        construct its GUI and pack it into the container.
+        """
+        pass
+
+    def redo(self, channel, image):
+        """This method is called when an image is set in a channel."""
+        pass
+
+
+class LocalPlugin(BasePlugin):
+    """Class to handle a local plugin."""
     def __init__(self, fv, fitsimage):
-        super(LocalPlugin, self).__init__()
-        self.fv = fv
-        self.logger = fv.logger
+        super(LocalPlugin, self).__init__(fv)
         self.fitsimage = fitsimage
 
         # find our channel info
@@ -79,69 +91,40 @@ class LocalPlugin(object):
             # TO BE DEPRECATED
             self.chinfo = self.channel
 
-        # Holds GUI widgets
-        self.w = Bunch.Bunch()
-
     def modes_off(self):
-        # turn off any mode user may be in
+        """Turn off any mode user may be in."""
         bm = self.fitsimage.get_bindmap()
         bm.reset_mode(self.fitsimage)
 
     # def build_gui(self, container):
-    #     """If a plugin defines this method, it will be called with a
+    #     """
+    #     If a plugin defines this method, it will be called with a
     #     container object in which to build its GUI. It should finish
     #     by packing into this container.  This will be called every
     #     time the local plugin is activated.
     #     """
     #     pass
 
-    def start(self):
-        """This method is called just after build_gui() when the plugin is
-        activated.
-        """
-        pass
-
-    def stop(self):
-        """This method is called when the plugin is deactivated.
-        """
-        pass
-
     def pause(self):
-        """This method is called when the plugin is defocused.  The plugin
-        should disable any user input that it responds to.
+        """
+        This method is called when the plugin is defocused.
+        The plugin should disable any user input that it responds to.
         """
         pass
 
     def resume(self):
-        """This method is called when the plugin is focused.  The plugin
-        should enable any user input that it responds to.
+        """
+        This method is called when the plugin is focused.
+        The plugin should enable any user input that it responds to.
         """
         pass
 
     def redo(self):
-        """This method is called when a new image arrives in the channel
+        """
+        This method is called when a new image arrives in the channel
         associated with the plugin.  It can optionally redo whatever operation
         it is doing.
         """
         pass
-
-    def help(self):
-        name = str(self)
-
-        if not self.fv.gpmon.has_plugin('WBrowser'):
-            self.fv.help_text(name.capitalize(), self.__doc__, text_kind='rst',
-                              trim_pfx=4)
-            return
-
-        from ginga.doc.download_doc import get_doc
-
-        self.fv.start_global_plugin('WBrowser')
-
-        # need to let GUI finish processing, it seems
-        self.fv.update_pending()
-
-        obj = self.fv.gpmon.get_plugin('WBrowser')
-        url = get_doc(plugin=self)
-        obj.browse(url)
 
 # END
--- ginga/doc/download_doc.py
+++ ginga/doc/download_doc.py
@@ -1,22 +1,69 @@
 """Download rendered HTML doc from RTD."""
+from __future__ import absolute_import, division, print_function
+from ginga.util.six.moves import urllib
 
 import os
 import shutil
 import sys
 import zipfile
 
+from astropy.utils import minversion
 from astropy.utils.data import download_file, _find_pkg_data_path
 
 __all__ = ['get_doc']
 
 
-def _download_latest_zip(**kwargs):
+def _find_rtd_version():
+    """Find closest RTD doc version."""
+    vstr = 'latest'
+    try:
+        import ginga
+        from bs4 import BeautifulSoup
+    except ImportError:
+        return vstr
+
+    # No active doc build before this release, just use latest.
+    if not minversion(ginga, '2.6.0'):
+        return vstr
+
+    # Get RTD download listing.
+    with urllib.request.urlopen(
+            'https://readthedocs.org/projects/ginga/downloads/') as r:
+        soup = BeautifulSoup(r, 'html.parser')
+
+    # Compile a list of available HTML doc versions for download.
+    all_rtd_vernums = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if 'htmlzip' not in href:
+            continue
+        s = href.split('/')[-2]
+        if s.startswith('v'):  # Ignore latest and stable
+            all_rtd_vernums.append(s)
+    all_rtd_vernums.sort(reverse=True)
+
+    # Find closest match.
+    ginga_ver = ginga.__version__
+    for rtd_ver in all_rtd_vernums:
+        if ginga_ver > rtd_ver[1:]:  # Ignore "v" in comparison
+            break
+        else:
+            vstr = rtd_ver
+
+    return vstr
+
+
+def _download_rtd_zip(rtd_version=None, **kwargs):
     """
-    Download and extract latest HTML ZIP from RTD to installed doc data path.
+    Download and extract HTML ZIP from RTD to installed doc data path.
     Download is skipped if content already exists.
 
     Parameters
     ----------
+    rtd_version : str or `None`
+        RTD version to download; e.g., "latest", "stable", or "v2.6.0".
+        If not given, download closest match to software version.
+
     kwargs : dict
         Keywords for :func:`astropy.utils.data.download_file`.
 
@@ -30,14 +77,21 @@ def _download_latest_zip(**kwargs):
     if sys.platform.startswith('win'):
         raise OSError('Windows is not supported')
 
+    if rtd_version is None:
+        rtd_version = _find_rtd_version()
+
     data_path = os.path.dirname(
         _find_pkg_data_path('help.html', package='ginga.doc'))
     index_html = os.path.join(data_path, 'index.html')
 
+    # There is a previous download of documentation; Do nothing.
+    # There is no check if downloaded version is outdated; The idea is that
+    # this folder would be empty again when installing new version.
     if os.path.isfile(index_html):
         return index_html
 
-    url = 'https://readthedocs.org/projects/ginga/downloads/htmlzip/latest/'
+    url = ('https://readthedocs.org/projects/ginga/downloads/htmlzip/'
+           '{}/'.format(rtd_version))
     local_path = download_file(url, **kwargs)
 
     with zipfile.ZipFile(local_path, 'r') as zf:
@@ -45,7 +99,7 @@ def _download_latest_zip(**kwargs):
 
     # RTD makes an undesirable sub-directory, so move everything there
     # up one level and delete it.
-    subdir = os.path.join(data_path, 'ginga-latest')
+    subdir = os.path.join(data_path, 'ginga-{}'.format(rtd_version))
     for s in os.listdir(subdir):
         src = os.path.join(subdir, s)
         if os.path.isfile(src):
@@ -74,38 +128,41 @@ def get_doc(plugin=None):
 
     Returns
     -------
-    url : str
-        URL to local documentation, if available, or online.
+    url : str or `None`
+        URL to local documentation, if available.
 
     """
     from ginga.GingaPlugin import GlobalPlugin, LocalPlugin
 
     if isinstance(plugin, GlobalPlugin):
-        plugin_page = 'plugins_global'
+        # plugin_page = 'plugins_global'
         plugin_name = str(plugin)
     elif isinstance(plugin, LocalPlugin):
-        plugin_page = 'plugins_local'
+        # plugin_page = 'plugins_local'
         plugin_name = str(plugin)
     else:
-        plugin_page = None
+        # plugin_page = None
         plugin_name = None
 
     try:
-        index_html = _download_latest_zip()
+        index_html = _download_rtd_zip()
 
     # Download failed, use online resource
     except Exception:
         url = 'https://ginga.readthedocs.io/en/latest/'
 
         if plugin_name is not None:
-            url += '{}/{}.html'.format(plugin_page, plugin_name)
+            # This redirects to online doc.
+            # url += '{}/{}.html'.format(plugin_page, plugin_name)
+            # This displays plugin docstring.
+            url = None
 
     # Use local resource
-    # https://github.com/rtfd/readthedocs.org/issues/2803
     else:
         pfx = 'file:'
         url = '{}{}'.format(pfx, index_html)
 
+        # https://github.com/rtfd/readthedocs.org/issues/2803
         if plugin_name is not None:
             url += '#{}'.format(plugin_name)
 
