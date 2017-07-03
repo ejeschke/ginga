b04f3d0ad4a88511c4e60791ce7e5a35cac03d62
J. Xavier Prochaska <xavier@ucolick.org>
2017-07-03 05:52:28 -0700

    success

--- ginga/util/wcsmod/wcs_img.py
+++ ginga/util/wcsmod/wcs_img.py
@@ -7,36 +7,59 @@ from ginga.util.wcsmod import common
 
 coord_types = ['pixel']
 
-class BareBonesWCS(common.BaseWCS):
-    """A dummy placeholder WCS.
-
-    .. note::
-        To get WCS functionality, please install one of the 3rd party python
-        WCS modules referred to at the top of this module.
-
+class ImgWCS(common.BaseWCS):
+    """Use an image (or images) to provide a pseudo-WCS.
     """
-    def __init__(self, logger):
-        super(BareBonesWCS, self).__init__(logger)
-        self.kind = 'barebones'
-
-    def load_header(self, header, fobj=None):
+    def __init__(self, logger): #, shape):
+        super(ImgWCS, self).__init__(logger)
+        self.kind = 'imgwcs'
         self.coordsys = 'pixel'
 
-    def spectral_coord(self, idxs, coords='data'):
-        raise common.WCSError("This feature not supported by BareBonesWCS")
+    def load_header(self, header, fobj=None):
+        self.header = {}
+        self.header.update(header.items())
+        # Load WCS x-image
+        if 'WCS-XIMG' in self.header.keys():
+            from astropy.io import fits as pyfits
+            hdu = pyfits.open(self.header['WCS-XIMG'])
+            head0 = hdu[0].header
+            # Error check
+            if (head0['NAXIS1'] == header['NAXIS1']) & (head0['NAXIS2'] == header['NAXIS2']):
+                self.wcs_ximage = hdu[0].data
+                return
+        # Set dummy image if we get here
+        import numpy as np
+        try:
+            shape = self.header['NAXIS2'], self.header['NAXIS1']
+        except:
+            self.wcs_ximage = None
+        else:
+            self.wcs_ximage = np.outer(np.linspace(4000.,8000.,num=shape[0]), np.ones(shape[1]))
 
     def pixtoradec(self, idxs, coords='data'):
-        px_x, px_y = idxs[:2]
-        px_x, px_y = px_x + 1.0, px_y + 1.0
-        return (px_x, px_y)
+        """Convert a (x, y) pixel coordinate on the image to a (ra, dec)
+        coordinate in space.
 
-    def radectopix(self, px_x, px_y, coords='data', naxispath=None):
-        # px_x, px_y = px_x - 1.0, px_y - 1.0
-        return (px_x, px_y)
+        Parameter (coords):
+        - if 'data' then x, y coordinates are interpreted as 0-based
+        - otherwise coordinates are interpreted as 1-based (traditional FITS)
+        """
+        if self.wcs_ximage is None:
+            return 0., 0.
+        x, y = idxs[:2]
+
+        # account for DATA->FITS coordinate space
+        if coords == 'data':
+            x, y = int(round(x)), int(round(y))
+
+        ra_deg = self.wcs_ximage[y,x]
+        dec_deg = 0.
+
+        return ra_deg, dec_deg
 
     def pixtosystem(self, idxs, system=None, coords='data'):
         return self.pixtoradec(idxs, coords=coords)
 
 
 # register our WCS with ginga
-common.register_wcs('barebones', BareBonesWCS, coord_types)
+common.register_wcs('imgwcs', ImgWCS, coord_types)
