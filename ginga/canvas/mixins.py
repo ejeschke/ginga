bc62a0117944e6adbb6c7280d63d00bca83a7be0
Pey Lian Lim <lim@stsci.edu>
2017-10-13 16:09:22 -0400

    PEP8 fix for files under canvas sub-directory

--- ginga/canvas/mixins.py
+++ ginga/canvas/mixins.py
@@ -1,8 +1,8 @@
 #
 # convenience module for importing canvas mixin classes
 #
-from ginga.canvas.CompoundMixin import CompoundMixin
-from ginga.canvas.CanvasMixin import CanvasMixin
-from ginga.canvas.DrawingMixin import DrawingMixin
+from ginga.canvas.CompoundMixin import CompoundMixin  # noqa
+from ginga.canvas.CanvasMixin import CanvasMixin  # noqa
+from ginga.canvas.DrawingMixin import DrawingMixin  # noqa
 
-#END
+# END
