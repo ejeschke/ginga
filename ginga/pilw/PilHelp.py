5d63e5a027f2f515d56dd29e52437c7eaa06e90f
Pey Lian Lim <lim@stsci.edu>
2017-10-13 22:55:15 -0400

    PEP8 fix for files in pilw sub-directory

--- ginga/pilw/PilHelp.py
+++ ginga/pilw/PilHelp.py
@@ -4,13 +4,12 @@
 # This is open-source software licensed under a BSD license.
 # Please see the file LICENSE.txt for details.
 
-import os.path
 import numpy as np
-from PIL import Image, ImageFont, ImageDraw
+from PIL import ImageFont, ImageDraw
 
 from ginga import colors
 from ginga.fonts import font_asst
-from ginga.util.six.moves import map
+
 
 def get_cached_font(fontname, fontsize):
     key = (fontname, fontsize)
@@ -26,6 +25,7 @@ def get_cached_font(fontname, fontsize):
 
         return font
 
+
 def load_font(font_name, font_file):
     if not font_asst.have_font(font_name):
         font_asst.add_font(font_file, font_name=font_name)
@@ -38,11 +38,13 @@ class Pen(object):
         self.linewidth = linewidth
         self.alpha = alpha
 
+
 class Brush(object):
     def __init__(self, color='black', fill=False, alpha=1.0):
         self.color = color
         self.fill = fill
         self.alpha = alpha
+
 
 class Font(object):
     def __init__(self, fontname='Roboto', fontsize=12.0, color='black',
@@ -78,7 +80,7 @@ class PilContext(object):
         else:
             r, g, b = 1.0, 1.0, 1.0
 
-        return (int(r*255), int(g*255), int(b*255), int(alpha*255))
+        return (int(r * 255), int(g * 255), int(b * 255), int(alpha * 255))
 
     def get_pen(self, color, linewidth=1, alpha=1.0):
         # if hasattr(self, 'linestyle'):
@@ -100,7 +102,7 @@ class PilContext(object):
     def _cvt_points(self, points):
         # PIL seems to have trouble with numpy arrays as sequences
         # of points, so just convert to a list
-        return [ (p[0], p[1]) for p in points ]
+        return [(p[0], p[1]) for p in points]
 
     def text_extents(self, text, font):
         retval = self.ctx.textsize(text, font.font)
@@ -121,10 +123,12 @@ class PilContext(object):
         x, y = pt
         radius = int(radius)
         if (brush is not None) and brush.fill:
-            self.ctx.ellipse(((x-radius, y-radius), (x+radius, y+radius)),
+            self.ctx.ellipse(((x - radius, y - radius),
+                              (x + radius, y + radius)),
                              fill=brush.color, outline=pen.color)
         else:
-            self.ctx.ellipse(((x-radius, y-radius), (x+radius, y+radius)),
+            self.ctx.ellipse(((x - radius, y - radius),
+                              (x + radius, y + radius)),
                              outline=pen.color)
 
     def polygon(self, points, pen, brush):
