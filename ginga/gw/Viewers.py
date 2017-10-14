184ba4360bbf9d698ab7ff0c3f280d216309594d
Pey Lian Lim <lim@stsci.edu>
2017-10-13 21:31:48 -0400

    PEP8 fix for files under gw sub-directory

--- ginga/gw/Viewers.py
+++ ginga/gw/Viewers.py
@@ -2,15 +2,15 @@ from ginga import toolkit
 tkname = toolkit.get_family()
 
 if tkname == 'qt':
-    from ginga.qtw.Viewers import *
+    from ginga.qtw.Viewers import *  # noqa
 
 elif tkname == 'gtk':
-    from ginga.gtkw.Viewers import *
+    from ginga.gtkw.Viewers import *  # noqa
 
 elif tkname == 'gtk3':
-    from ginga.gtk3w.Viewers import *
+    from ginga.gtk3w.Viewers import *  # noqa
 
 elif tkname == 'pg':
-    from ginga.web.pgw.Viewers import *
+    from ginga.web.pgw.Viewers import *  # noqa
 
-from ginga.table.TableView import TableViewGw
+from ginga.table.TableView import TableViewGw  # noqa
