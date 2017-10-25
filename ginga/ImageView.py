0d56b485e2fd27ff5d6d1856c3c8eaf22cd9bb4a
Eric Jeschke <eric@naoj.org>
2017-10-25 07:15:52 -1000

    Moves some utility functions out of the base viewer class

--- ginga/ImageView.py
+++ ginga/ImageView.py
@@ -23,7 +23,7 @@ from ginga import RGBMap, AutoCuts, ColorDist
 from ginga import cmap, imap, colors, trcalc
 from ginga.canvas import coordmap, transform
 from ginga.canvas.types.layer import DrawingCanvas
-from ginga.util import rgb_cms
+from ginga.util import rgb_cms, addons
 
 __all__ = ['ImageViewBase']
 
@@ -3415,102 +3415,6 @@ class ImageViewBase(Callback.Callbacks):
         """
         raise ImageViewError("Subclass should override this abstract method!")
 
-    def show_pan_mark(self, tf, color='red'):
-        """Show a mark in the pan position (center of window).
-
-        Parameters
-        ----------
-        tf : bool
-            If True, show the mark; else remove it if present.
-
-        color : str
-            Color of the mark; default is 'red'.
-        """
-        tag = '_$pan_mark'
-        radius = 10
-
-        canvas = self.get_private_canvas()
-        try:
-            mark = canvas.get_object_by_tag(tag)
-            if not tf:
-                canvas.delete_object_by_tag(tag)
-            else:
-                mark.color = color
-
-        except KeyError:
-            if tf:
-                Point = canvas.get_draw_class('point')
-                canvas.add(Point(0, 0, radius, style='plus', color=color,
-                                 coord='cartesian'),
-                           tag=tag, redraw=False)
-
-        canvas.update_canvas(whence=3)
-
-    def show_mode_indicator(self, tf, corner='ur'):
-        """Show a keyboard mode indicator in one of the corners.
-
-        Parameters
-        ----------
-        tf : bool
-            If True, show the mark; else remove it if present.
-
-        corner : str
-            One of 'll', 'lr', 'ul' or 'ur' selecting a corner.
-            The default is 'ur'.
-
-        """
-        tag = '_$mode_indicator'
-
-        canvas = self.get_private_canvas()
-        try:
-            indic = canvas.get_object_by_tag(tag)
-            if not tf:
-                canvas.delete_object_by_tag(tag)
-            else:
-                indic.corner = corner
-
-        except KeyError:
-            if tf:
-                # force a redraw if the mode changes
-                bm = self.get_bindmap()
-                bm.add_callback('mode-set',
-                                lambda *args: self.redraw(whence=3))
-
-                Indicator = canvas.get_draw_class('modeindicator')
-                canvas.add(Indicator(corner=corner),
-                           tag=tag, redraw=False)
-
-        canvas.update_canvas(whence=3)
-
-    def show_color_bar(self, tf, side='bottom'):
-        """Show a color bar in the window.
-
-        Parameters
-        ----------
-        tf : bool
-            If True, show the color bar; else remove it if present.
-
-        side : str
-            One of 'top' or 'bottom'. The default is 'bottom'.
-
-        """
-
-        tag = '_$color_bar'
-        canvas = self.get_private_canvas()
-        try:
-            cbar = canvas.get_object_by_tag(tag)
-            if not tf:
-                canvas.delete_object_by_tag(tag)
-            else:
-                cbar.side = side
-
-        except KeyError:
-            if tf:
-                Cbar = canvas.get_draw_class('colorbar')
-                canvas.add(Cbar(side=side), tag=tag, redraw=False)
-
-        canvas.update_canvas(whence=3)
-
     def set_onscreen_message(self, text, redraw=True):
         """Called by a subclass to update the onscreen message.
 
@@ -3556,36 +3460,21 @@ class ImageViewBase(Callback.Callbacks):
         if redraw:
             canvas.update_canvas(whence=3)
 
+    def show_pan_mark(self, tf, color='red'):
+        # TO BE DEPRECATED--please use addons.show_pan_mark
+        addons.show_pan_mark(self, tf, color=color)
+
+    def show_mode_indicator(self, tf, corner='ur'):
+        # TO BE DEPRECATED--please use addons.show_mode_indicator
+        addons.show_mode_indicator(self, tf, corner=corner)
+
+    def show_color_bar(self, tf, side='bottom'):
+        # TO BE DEPRECATED--please use addons.show_color_bar
+        addons.show_color_bar(self, tf, side=side)
+
     def show_focus_indicator(self, tf, color='white'):
-        """Show a focus indicator in the window.
-
-        Parameters
-        ----------
-        tf : bool
-            If True, show the color bar; else remove it if present.
-
-        color : str
-            Color for the focus indicator.
-
-        """
-
-        tag = '_$focus_indicator'
-        canvas = self.get_private_canvas()
-        try:
-            fcsi = canvas.get_object_by_tag(tag)
-            if not tf:
-                canvas.delete_object_by_tag(tag)
-            else:
-                fcsi.color = color
-
-        except KeyError:
-            if tf:
-                Fcsi = canvas.get_draw_class('focusindicator')
-                fcsi = Fcsi(color=color)
-                canvas.add(fcsi, tag=tag, redraw=False)
-                self.add_callback('focus', fcsi.focus_cb)
-
-        canvas.update_canvas(whence=3)
+        # TO BE DEPRECATED--please use addons.show_focus_indicator
+        addons.show_focus_indicator(self, tf, color=color)
 
 
 class SuppressRedraw(object):
