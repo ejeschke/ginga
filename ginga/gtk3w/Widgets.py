d7d6f407f289564bb6b16aa0a4008a529660f618
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2017-11-16 16:55:19 +0800

    Correct the bug of require WebKit name for GTK3 backend
    The package WebKit is named as WebKit2 in Ubuntu distribution.

--- ginga/gtk3w/Widgets.py
+++ ginga/gtk3w/Widgets.py
@@ -22,7 +22,7 @@ has_webkit = False
 try:
     # this is necessary to prevent a warning message on import
     import gi
-    gi.require_version('WebKit', '3.0')
+    gi.require_version('WebKit2', '4.0')
 
     from gi.repository import WebKit  # noqa
     has_webkit = True
