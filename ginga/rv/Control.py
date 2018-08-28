53b5c3a9dcd01ff60e6fb592176401f2a9d6f39f
Eric Jeschke <eric@naoj.org>
2018-08-28 12:07:02 -1000

    Fix for missing method name

--- ginga/rv/Control.py
+++ ginga/rv/Control.py
@@ -281,11 +281,12 @@ class GingaShell(GwMain.GwMain, Widgets.Application):
         opmon = channel.opmon
         opmon.deactivate(opname)
 
-    def call_local_plugin_method(self, chname, plugin_name, args, kwargs):
+    def call_local_plugin_method(self, chname, plugin_name, method_name,
+                                 args, kwargs):
         channel = self.get_channel(chname)
         opmon = channel.opmon
         p_obj = opmon.get_plugin(plugin_name)
-        method = getattr(p_obj, plugin_name)
+        method = getattr(p_obj, method_name)
         return self.gui_call(method, *args, **kwargs)
 
     def start_global_plugin(self, plugin_name, raise_tab=False):
@@ -297,9 +298,10 @@ class GingaShell(GwMain.GwMain, Widgets.Application):
     def stop_global_plugin(self, plugin_name):
         self.gpmon.deactivate(plugin_name)
 
-    def call_global_plugin_method(self, plugin_name, args, kwargs):
+    def call_global_plugin_method(self, plugin_name, method_name,
+                                  args, kwargs):
         p_obj = self.gpmon.get_plugin(plugin_name)
-        method = getattr(p_obj, plugin_name)
+        method = getattr(p_obj, method_name)
         return self.gui_call(method, *args, **kwargs)
 
     def start_plugin(self, plugin_name, spec):
