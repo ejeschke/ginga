043d974bee5ac7e26f0b700ac1475414ad44f0e4
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2021-03-04 10:56:25 -0500

    MNT: Remove duplicate license

--- ginga/__init__.py
+++ ginga/__init__.py
@@ -1,4 +1,4 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# Licensed under a 3-clause BSD style license - see LICENSE.txt
 """See LONG_DESC.txt"""
 
 # Packages may add whatever they like to this file, but
