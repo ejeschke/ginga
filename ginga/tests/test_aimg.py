e0f858653e666b39268744c7e7a7f544c67ba5d5
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2021-04-28 17:54:35 -0400

    MNT: Revert #938
    as the datfix warning should no longer be issued
    because astropy updated bundled wcslib.

--- ginga/tests/test_aimg.py
+++ ginga/tests/test_aimg.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pytest
 
 from astropy import nddata
 from astropy.io import fits
@@ -11,7 +10,6 @@ from ginga.util import wcs, wcsmod
 wcsmod.use('astropy')
 
 
-@pytest.mark.filterwarnings("ignore:'datfix' made the change")
 class TestAstroImage(object):
     def setup_class(self):
         self.logger = log.get_logger("TestAstroImage", null=True)
