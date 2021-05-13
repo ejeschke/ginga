32682fe0d8addf472fbf1af52dfec9b7c6dd367c
Eric Jeschke <eric@naoj.org>
2021-05-12 19:56:25 -1000

    Fix tests numpy warning

--- ginga/tests/test_XYDataSource.py
+++ ginga/tests/test_XYDataSource.py
@@ -11,8 +11,8 @@ class TestXYDataSource(object):
 
     def test_initialize_points(self):
         n = 7
-        expected = np.array([(x, x**2) for x in range(0, n)], dtype=np.int)
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        expected = np.array([(x, x**2) for x in range(0, n)], dtype=int)
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=expected)
 
         actual = dsrc.points
@@ -20,9 +20,9 @@ class TestXYDataSource(object):
 
     def test_set_points(self):
         n = 7
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int))
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int))
 
-        expected = np.array([(x, x**2) for x in range(0, n)], dtype=np.int)
+        expected = np.array([(x, x**2) for x in range(0, n)], dtype=int)
         dsrc.set_points(expected)
 
         actual = dsrc.get_points()
@@ -30,8 +30,8 @@ class TestXYDataSource(object):
 
     def test_full_exception(self):
         n = 7
-        expected = np.array([(x, x**2) for x in range(0, n)], dtype=np.int)
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        expected = np.array([(x, x**2) for x in range(0, n)], dtype=int)
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=expected, overwrite=False)
 
         with pytest.raises(ValueError):
@@ -39,8 +39,8 @@ class TestXYDataSource(object):
 
     def test_is_full(self):
         n = 7
-        expected = np.array([(x, x**2) for x in range(0, n)], dtype=np.int)
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        expected = np.array([(x, x**2) for x in range(0, n)], dtype=int)
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=expected, overwrite=False)
 
         assert dsrc.is_fullp()
@@ -48,34 +48,34 @@ class TestXYDataSource(object):
     def test_overwrite(self):
         n = 7
         points = [(x, x**2) for x in range(0, n)]
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=points, overwrite=True)
 
         to_add = [(8, 8**2), (9, 9**2)]
         for pt in to_add:
             dsrc.add(pt)
 
-        expected = np.array(points[len(to_add):] + to_add, dtype=np.int)
+        expected = np.array(points[len(to_add):] + to_add, dtype=int)
         actual = dsrc.get_points()
         assert_allclose(actual, expected)
 
     def test_add_points(self):
         n = 7
         points = [(x, x**2) for x in range(0, n)]
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=points, overwrite=True)
 
         to_add = [(8, 8**2), (9, 9**2)]
         dsrc.add_points(to_add)
 
-        expected = np.array(points[len(to_add):] + to_add, dtype=np.int)
+        expected = np.array(points[len(to_add):] + to_add, dtype=int)
         actual = dsrc.get_points()
         assert_allclose(actual, expected)
 
     def test_peek(self):
         n = 7
         points = [(x, x**2) for x in range(0, n)]
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=points, overwrite=True)
 
         expected = points[-1]
@@ -85,7 +85,7 @@ class TestXYDataSource(object):
     def test_peek_rear(self):
         n = 7
         points = [(x, x**2) for x in range(0, n)]
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=points, overwrite=True)
 
         expected = points[0]
@@ -95,7 +95,7 @@ class TestXYDataSource(object):
     def test_pop(self):
         n = 7
         points = [(x, x**2) for x in range(0, n)]
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=points, overwrite=True)
 
         expected = points[-1]
@@ -107,7 +107,7 @@ class TestXYDataSource(object):
     def test_pop_rear(self):
         n = 7
         points = [(x, x**2) for x in range(0, n)]
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=points, overwrite=True)
 
         expected = points[0]
@@ -118,8 +118,8 @@ class TestXYDataSource(object):
 
     def test_points_property(self):
         n = 7
-        expected = np.array([(x, x**2) for x in range(0, n)], dtype=np.int)
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        expected = np.array([(x, x**2) for x in range(0, n)], dtype=int)
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=expected, overwrite=True)
 
         actual = dsrc.points
@@ -132,7 +132,7 @@ class TestXYDataSource(object):
         y_arr = np.random.randint(0, high=1000, size=n)
         x_arr = np.arange(0, n)
         points = np.array((x_arr, y_arr)).T
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             points=points, overwrite=False)
 
         expected = np.array([(x_arr.min(), y_arr.min()),
@@ -142,7 +142,7 @@ class TestXYDataSource(object):
 
     def test_empty_exception(self):
         n = 7
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             none_for_empty=False)
 
         with pytest.raises(ValueError):
@@ -159,7 +159,7 @@ class TestXYDataSource(object):
 
     def test_none_for_empty(self):
         n = 7
-        dsrc = XYDataSource(np.zeros((n, 2), dtype=np.int),
+        dsrc = XYDataSource(np.zeros((n, 2), dtype=int),
                             none_for_empty=True)
         expected = None
         actual = dsrc.peek()
