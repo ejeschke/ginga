cf28f4da3fbe658a3203d0c4cf4e78ec403decae
Eric Jeschke <eric@naoj.org>
2021-06-11 11:50:17 -1000

    More review recommendations

--- ginga/__init__.py
+++ ginga/__init__.py
@@ -7,15 +7,4 @@
 from ._astropy_init import *  # noqa
 # ----------------------------------------------------------------------------
 
-try:
-    # As long as we're using setuptools/distribute, we need to do this the
-    # setuptools way or else pkg_resources will throw up unnecessary and
-    # annoying warnings (even though the namespace mechanism will still
-    # otherwise work without it).
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    pass
-
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)  # noqa
-
 # END
