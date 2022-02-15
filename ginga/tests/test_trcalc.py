1e118619738ae338cb2a42b1332e9343d4ee2506
Eric Jeschke <eric@naoj.org>
2022-02-14 17:53:19 -1000

    fix typecheck tests for trcalc module

--- ginga/tests/test_trcalc.py
+++ ginga/tests/test_trcalc.py
@@ -36,7 +36,7 @@ class TestTrcalc:
                                                           nd[0], nd[1])
         new_data = trcalc.fancy_index(data, view)
         assert new_data.shape == (10, 8)
-        assert isinstance(new_data[0, 0], int)
+        assert isinstance(new_data[0, 0], np.integer)
         res = 'dc025d4e14db5529c581cbe24f0616721bb33f63aabcfcc0d432edf00d8cdc2d'
         m = hashlib.sha256()
         m.update(str(new_data.tolist()).encode())
@@ -53,7 +53,7 @@ class TestTrcalc:
                                                             p1, p2, nd)
         new_data = trcalc.fancy_index(data, view)
         assert new_data.shape == (4, 4, 4)
-        assert isinstance(new_data[0, 0, 0], int)
+        assert isinstance(new_data[0, 0, 0], np.integer)
         res = 'c01c00af06fb2dc5c8cd6cf96927ba6ddd8d2caba3fc33074c9eaab5cc0ac498'
         m = hashlib.sha256()
         m.update(str(new_data.tolist()).encode())
