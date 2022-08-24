3ca5bcd130f6fca97af987a4f035a03e8e84e383
Eric Jeschke <eric@naoj.org>
2022-08-23 18:58:44 -1000

    Add mode discovery from $HOME/.ginga/modes folder

--- ginga/modes/modeinfo.py
+++ ginga/modes/modeinfo.py
@@ -3,12 +3,18 @@
 # Please see the file LICENSE.txt for details.
 #
 import sys
+import os
+import glob
+import inspect
 if sys.version_info < (3, 8):
     # Python 3.7
     from importlib_metadata import entry_points
 else:
     from importlib.metadata import entry_points
 
+from ginga.util import paths
+from ginga.modes.mode_base import Mode
+from ginga.misc.ModuleManager import my_import
 
 available_modes = []
 
@@ -19,6 +25,7 @@ def add_mode(mode_class):
 
 
 def discover_modes():
+    # discover modes installed under entry point "ginga_modes"
     group = 'ginga_modes'
     discovered_modes = entry_points().get(group, [])
     for entry_point in discovered_modes:
@@ -29,6 +36,24 @@ def discover_modes():
         except Exception as e:
             print("Error trying to load entry point %s: %s" % (
                 str(entry_point), str(e)))
+
+    # discover modes in the user's $HOME/.ginga/modes folder
+    moduledir = os.path.join(paths.ginga_home, "modes")
+    if os.path.isdir(moduledir):
+        files = glob.glob(os.path.join(moduledir, "*.py"))
+        for path in files:
+            _dir, fname = os.path.split(path)
+            fname, _sfx = os.path.splitext(fname)
+            module = my_import(fname, path=path)
+            for attr in module.__dict__.values():
+                if (inspect.isclass(attr) and issubclass(attr, Mode) and
+                    attr is not Mode):
+                    try:
+                        add_mode(attr)
+
+                    except Exception as e:
+                        print("Error trying to load mode %s: %s" % (
+                            fname, str(e)))
 
 
 if len(available_modes) == 0:
