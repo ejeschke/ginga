ejeschke/ginga

commit 3187f93e8da43a419766e6322a5d3868514834fc

Fix GridBox routines
- fix GridBox routines in pg widgets backend
- add corresponding routines to gtk3 and qt backends
- insert_row(), append_row(), delete_row()
- fix behavior of add_widget() to remove old child cleanly when adding
  a new one at the same row/column

ginga/gtk3w/Widgets.py
@@ -1826,8 +1826,10 @@ class GridBox(ContainerBase):
     def __init__(self, rows=1, columns=1):
         super(GridBox, self).__init__()
 
+        # TODO: GtkTable has been deprecated--migrate to GtkGrid?
         w = Gtk.Table(rows=rows, columns=columns)
         self.widget = w
+        self.tbl = {}
         self.num_rows = rows
         self.num_cols = columns
 
@@ -1846,6 +1848,9 @@ class GridBox(ContainerBase):
         self.set_row_spacing(val)
         self.set_column_spacing(val)
 
+    def get_row_column_count(self):
+        return self.num_rows, self.num_cols
+
     def add_widget(self, child, row, col, stretch=0):
         resize = False
         if row > self.num_rows:
@@ -1857,6 +1862,13 @@ class GridBox(ContainerBase):
         if resize:
             self.resize_grid(self.num_rows, self.num_cols)
 
+        key = (row, col)
+        if key in self.tbl:
+            # take care of case where we are overwriting a child
+            old_child = self.tbl[key]
+            old_child.hide()
+            self.remove(old_child)
+        self.tbl[key] = child
         self.add_ref(child)
         w = child.get_widget()
         if stretch > 0:
@@ -1872,6 +1884,87 @@ class GridBox(ContainerBase):
                            xpadding=0, ypadding=0)
         self.widget.show_all()
         self.make_callback('widget-added', child)
+
+    def get_widget_at_cell(self, row, col):
+        return self.tbl[(row, col)]
+
+    def insert_row(self, index, widgets):
+        if len(widgets) != self.num_cols:
+            raise ValueError("Number of widgets (%d) != number of columns (%d)".format(len(widgets), self.num_cols))
+
+        self.num_rows += 1
+        self.resize_grid(self.num_rows, self.num_cols)
+
+        xoptions = (Gtk.AttachOptions.FILL | Gtk.AttachOptions.SHRINK)
+        yoptions = (Gtk.AttachOptions.FILL | Gtk.AttachOptions.SHRINK)
+
+        # handle case where user inserts row before the end of the gridbox
+        if index < self.num_rows - 1:
+            # shift key/value pairs down to make the row empty at index
+            for i in range(self.num_rows - 2, index - 1, -1):
+                for j in range(self.num_cols):
+                    child = self.tbl[(i, j)]
+                    self.tbl[(i + 1, j)] = child
+                    # move actual widget down in GtkTable
+                    w = child.get_widget()
+                    self._remove(w)
+                    row, col = i + 1, j
+                    self.widget.attach(w, col, col + 1, row, row + 1,
+                                       xoptions=xoptions, yoptions=yoptions,
+                                       xpadding=0, ypadding=0)
+
+        for j in range(self.num_cols):
+            child = widgets[j]
+            # populate inserted row with widgets
+            self.tbl[(index, j)] = child
+            self.add_ref(child)
+            row, col = index, j
+            w = child.get_widget()
+            self.widget.attach(w, col, col + 1, row, row + 1,
+                               xoptions=xoptions, yoptions=yoptions,
+                               xpadding=0, ypadding=0)
+
+        self.widget.show_all()
+
+        for child in widgets:
+            self.make_callback('widget-added', child)
+
+    def append_row(self, widgets):
+        num_rows, num_cols = self.get_row_column_count()
+        return self.insert_row(num_rows, widgets)
+
+    def delete_row(self, index):
+        if index < 0 or index >= self.num_rows:
+            raise ValueError("Index (%d) out of bounds (%d)".format(index, self.num_rows))
+
+        xoptions = (Gtk.AttachOptions.FILL | Gtk.AttachOptions.SHRINK)
+        yoptions = (Gtk.AttachOptions.FILL | Gtk.AttachOptions.SHRINK)
+
+        # remove widgets in row from table
+        for j in range(self.num_cols):
+            child = self.tbl.pop((index, j))
+            self.remove(child)
+
+        if index < self.num_rows - 1:
+            # if not removing very last row,
+            # shift dict key, value pairs up
+            for i in range(index + 1, self.num_rows):
+                for j in range(self.num_cols):
+                    child = self.tbl[(i, j)]
+                    self.tbl[(i - 1, j)] = child
+                    # move actual widget up in GtkTable
+                    w = child.get_widget()
+                    self._remove(w)
+                    row, col = i - 1, j
+                    self.widget.attach(w, col, col + 1, row, row + 1,
+                                       xoptions=xoptions, yoptions=yoptions,
+                                       xpadding=0, ypadding=0)
+            # delete items in last row to maintain self.tbl
+            for j in range(self.num_cols):
+                self.tbl.pop((self.num_rows - 1, j))
+
+        self.num_rows -= 1
+        self.resize_grid(self.num_rows, self.num_cols)
 
 
 class Toolbar(ContainerBase):

ginga/qtw/Widgets.py
@@ -5,7 +5,6 @@
 # Please see the file LICENSE.txt for details.
 #
 import os.path
-import platform
 import pathlib
 from functools import reduce
 
@@ -89,7 +88,7 @@ class WidgetBase(Callback.Callbacks):
         # self.widget.raise_()
 
     def resize(self, width, height):
-        self.widget.resize(int(width), int(height))
+        self.widget.resize(width, height)
 
     def show(self):
         self.widget.show()
@@ -1597,6 +1596,7 @@ class GridBox(ContainerBase):
         layout = QtGui.QGridLayout()
         w.setLayout(layout)
         self.widget = w
+        self.tbl = {}
 
     def resize_grid(self, rows, columns):
         pass
@@ -1611,23 +1611,86 @@ class GridBox(ContainerBase):
         self.set_row_spacing(val)
         self.set_column_spacing(val)
 
+    def get_row_column_count(self):
+        num_rows = self.widget.layout().rowCount()
+        num_cols = self.widget.layout().columnCount()
+        return num_rows, num_cols
+
     def add_widget(self, child, row, col, stretch=0):
+        key = (row, col)
+        if key in self.tbl:
+            # take care of case where we are overwriting a child
+            old_child = self.tbl[key]
+            old_child.hide()
+            self.remove(old_child)
+        self.tbl[key] = child
         self.add_ref(child)
+
         w = child.get_widget()
         self.widget.layout().addWidget(w, row, col)
         self.make_callback('widget-added', child)
 
-    def insert_cell(self, row, col):
-        raise NotImplementedError("insert_cell needs to be implemented!")
+    def get_widget_at_cell(self, row, col):
+        return self.tbl[(row, col)]
 
-    def insert_row(self, index):
-        raise NotImplementedError("insert_row needs to be implemented!")
+    def insert_row(self, index, widgets):
+        num_rows, num_cols = self.get_row_column_count()
 
-    def append_row(self):
-        raise NotImplementedError("append_row needs to be implemented!")
+        if len(widgets) != num_cols:
+            raise ValueError("Number of widgets (%d) != number of columns (%d)".format(len(widgets), num_cols))
+
+        # handle case where user inserts row before the end of the gridbox
+        if index < num_rows:
+            # shift key/value pairs down to make the row empty at index
+            for i in range(num_rows, index, -1):
+                for j in range(num_cols):
+                    key = (i - 1, j)
+                    if key in self.tbl:
+                        child = self.tbl.pop(key)
+                        self.tbl[(i, j)] = child
+                        # move actual widget down in QGridLayout
+                        w = child.get_widget()
+                        self._remove(w)
+                        self.widget.layout().addWidget(w, i, j)
+
+        for j in range(num_cols):
+            child = widgets[j]
+            self.add_widget(child, index, j)
+
+    def append_row(self, widgets):
+        num_rows, num_cols = self.get_row_column_count()
+        return self.insert_row(num_rows, widgets)
 
     def delete_row(self, index):
-        raise NotImplementedError("delete_row needs to be implemented!")
+        num_rows, num_cols = self.get_row_column_count()
+        if index < 0 or index >= num_rows:
+            raise ValueError("Index (%d) out of bounds (%d)".format(index, num_rows))
+
+        # remove widgets in row to be deleted from table
+        for j in range(num_cols):
+            key = (index, j)
+            if key in self.tbl:
+                child = self.tbl.pop(key)
+                child.hide()
+                self.remove(child)
+
+        if index < num_rows - 1:
+            # if not removing very last row, shift key/value pairs up
+            for i in range(index + 1, num_rows):
+                for j in range(num_cols):
+                    key = (i, j)
+                    if key in self.tbl:
+                        child = self.tbl[key]
+                        self.tbl[(i - 1, j)] = child
+                        # move actual widget up in QGridLayout
+                        w = child.get_widget()
+                        self._remove(w)
+                        self.widget.layout().addWidget(w, i - 1, j)
+            # delete items in last row to maintain self.tbl
+            for j in range(num_cols):
+                key = (num_rows - 1, j)
+                if key in self.tbl:
+                    self.tbl.pop(key)
 
 
 class ToolbarAction(WidgetBase):
@@ -1793,9 +1856,8 @@ class Menubar(ContainerBase):
         super(Menubar, self).__init__()
 
         self.widget = QtGui.QMenuBar()
-        macos_ver = platform.mac_ver()[0]
-        if len(macos_ver) > 0:
-            self.widget.setNativeMenuBar(True)
+        if hasattr(self.widget, 'setNativeMenuBar'):
+            self.widget.setNativeMenuBar(False)
         self.menus = Bunch.Bunch(caseless=True)
 
     def add_widget(self, child, name):

ginga/web/pgw/Widgets.py
@@ -2392,32 +2392,26 @@ class GridBox(ContainerBase):
         %(content)s
     </table>
     <script type="text/javascript">
-        // see python method insert_cell in this widget
-        ginga_app.add_widget_custom_method('%(id)s', 'insert_cell',
-                function (elt, msg) {
-                    let table = document.getElementById("%(id)s");
-                    let row = msg.value[0];
-                    table.rows.item(row).insertCell(msg.value[1]);
-        });
         // see python method insert_row in this widget
         ginga_app.add_widget_custom_method('%(id)s', 'insert_row',
                 function (elt, msg) {
                     let index = msg.value[0];
                     let numColumns = msg.value[1];
                     let newRow = document.getElementById("%(id)s").insertRow(index);
-                    for (let i = 0; i < numColumns; i++) {
-                        newRow.insertCell(i);
+                    for (let j = 0; j < numColumns; j++) {
+                        let cell = newRow.insertCell(j);
+                        cell.innerHTML = msg.value[2][j];
                     }
         });
-        // see python method append_row in this widget
-        ginga_app.add_widget_custom_method('%(id)s', 'append_row',
+        // see python method update_cell in this widget
+        ginga_app.add_widget_custom_method('%(id)s', 'update_cell',
                 function (elt, msg) {
-                    let numRows = msg.value[0];
-                    let numColumns = msg.value[1];
-                    let newRow = document.getElementById("%(id)s").insertRow(numRows);
-                    for (let i = 0; i < numColumns; i++) {
-                        newRow.insertCell(i);
-                    }
+                    let i = msg.value[0];
+                    let j = msg.value[1];
+                    let rows = document.getElementById("%(id)s").rows;
+                    let row = rows[i];
+                    let cell = row.cells[j];
+                    cell.innerHTML = msg.value[2];
         });
         // see python method delete_row in this widget
         ginga_app.add_widget_custom_method('%(id)s', 'delete_row',
@@ -2438,6 +2432,8 @@ class GridBox(ContainerBase):
         self.tbl = {}
 
     def resize_grid(self, rows, columns):
+        # TODO: this needs to manipulate self.tbl and self.children
+        # if the size is being reduced
         self.num_rows = rows
         self.num_cols = columns
 
@@ -2454,6 +2450,9 @@ class GridBox(ContainerBase):
         self.set_row_spacing(val)
         self.set_column_spacing(val)
 
+    def get_row_column_count(self):
+        return self.num_rows, self.num_cols
+
     def set_column_spacing(self, val):
         self.col_spacing = val
         self.add_css_styles([('border-collapse', 'separate')])
@@ -2464,64 +2463,69 @@ class GridBox(ContainerBase):
             app.do_operation("update_style", id=self.id, value=style)
 
     def add_widget(self, child, row, col, stretch=0):
-        self.add_ref(child)
         self.num_rows = max(self.num_rows, row + 1)
         self.num_cols = max(self.num_cols, col + 1)
-        self.tbl[(row, col)] = child
+        key = (row, col)
+        if key in self.tbl:
+            # take care of case where we are overwriting a child
+            old_child = self.tbl[key]
+            self.remove(old_child)
+        self.tbl[key] = child
+        self.add_ref(child)
 
         if self._rendered:
             app = self.get_app()
-            app.do_operation('update_html', id=self.id,
-                             value=self.render_body())
+            app.do_operation('update_cell', id=self.id,
+                             value=[row, col, child.render()])
         self.make_callback('widget-added', child)
 
-    def insert_cell(self, row, col):
-        indices = [row, col]
-        if self._rendered:
-            app = self.get_app()
-            app.do_operation("insert_cell", id=self.id, value=indices)
+    def get_widget_at_cell(self, row, col):
+        return self.tbl[(row, col)]
 
-    def insert_row(self, index):
-        indices = [index, self.num_cols]
+    def insert_row(self, index, widgets):
+        if len(widgets) != self.num_cols:
+            raise ValueError("Number of widgets (%d) != number of columns (%d)".format(len(widgets), self.num_cols))
+
         self.num_rows += 1
 
-        # handle case where user inserts row at the end of the gridbox
-        if index == self.num_rows - 1:
-            for j in range(self.num_cols):
-                self.tbl[(index, j)] = Box()
-        else:
+        # handle case where user inserts row before the end of the gridbox
+        if index < self.num_rows - 1:
             # shift key/value pairs down to make the row empty at index
             for i in range(self.num_rows - 2, index - 1, -1):
                 for j in range(self.num_cols):
                     self.tbl[(i + 1, j)] = self.tbl[(i, j)]
-            # populate inserted row with empty Boxes for render_body()
-            for j in range(self.num_cols):
-                self.tbl[(index, j)] = Box()
 
-        if self._rendered:
-            app = self.get_app()
-            app.do_operation("insert_row", id=self.id, value=indices)
-
-    def append_row(self):
-        indices = [self.num_rows, self.num_cols]
-        if self._rendered:
-            app = self.get_app()
-            app.do_operation("append_row", id=self.id, value=indices)
-        self.num_rows += 1
-        # populate appended row with empty Boxes for render_body()
+        html = []
         for j in range(self.num_cols):
-            self.tbl[(self.num_rows - 1, j)] = Box()
+            child = widgets[j]
+            # populate inserted row with widgets for render_body()
+            self.tbl[(index, j)] = widgets[j]
+            self.add_ref(child)
+            if self._rendered:
+                html.append(child.render())
+
+        if self._rendered:
+            app = self.get_app()
+            args = [index, self.num_cols, html]
+            app.do_operation("insert_row", id=self.id, value=args)
+
+        for child in widgets:
+            self.make_callback('widget-added', child)
+
+    def append_row(self, widgets):
+        return self.insert_row(self.num_rows, widgets)
 
     def delete_row(self, index):
-
         if index < 0 or index >= self.num_rows:
-            print("Index out of bounds")
-            return
+            raise ValueError("Index (%d) out of bounds (%d)".format(index, self.num_rows))
 
-        if index == self.num_rows - 1:
-            for j in range(self.num_cols):
-                self.tbl.pop((self.num_rows - 1, j))
-        else:
+        # remove widgets in row from table
+        for j in range(self.num_cols):
+            child = self.tbl.pop((index, j))
+            self.remove(child)
+
+        if index < self.num_rows - 1:
+            # if not removing very last row,
             # shift dict key, value pairs up
             for i in range(index + 1, self.num_rows):
                 for j in range(self.num_cols):

commit 804ca99f8bde9f3bc75220b26b3020e9367bc5f9

Fix bug with remove() method in GridBox
- fixes an issue where widgets weren't fully removed due to a change
  introduced in commit 3187f93e8da43a419766e6322a5d3868514834fc

ginga/gtk3w/Widgets.py
@@ -1885,6 +1885,16 @@ class GridBox(ContainerBase):
         self.widget.show_all()
         self.make_callback('widget-added', child)
 
+    def remove(self, child, delete=False):
+        # need to delete the child from self.tbl
+        children = list(self.tbl.values())
+        keys = list(self.tbl.keys())
+        idx = children.index(child)
+        key = keys[idx]
+        del self.tbl[key]
+
+        super().remove(child, delete=delete)
+
     def get_widget_at_cell(self, row, col):
         return self.tbl[(row, col)]
 

ginga/gw/Desktop.py
@@ -1043,7 +1043,7 @@ class SymmetricGridWidget(Widgets.GridBox):
         try:
             idx = widgets.index(child)
 
-        except IndexError:
+        except (IndexError, ValueError):
             raise ValueError("child not in container")
 
         o_rows, o_cols = self._calc_dims(len(widgets))

ginga/qtw/Widgets.py
@@ -1630,6 +1630,16 @@ class GridBox(ContainerBase):
         self.widget.layout().addWidget(w, row, col)
         self.make_callback('widget-added', child)
 
+    def remove(self, child, delete=False):
+        # need to delete the child from self.tbl
+        children = list(self.tbl.values())
+        keys = list(self.tbl.keys())
+        idx = children.index(child)
+        key = keys[idx]
+        del self.tbl[key]
+
+        super().remove(child, delete=delete)
+
     def get_widget_at_cell(self, row, col):
         return self.tbl[(row, col)]
 

ginga/web/pgw/Widgets.py
@@ -2479,6 +2479,16 @@ class GridBox(ContainerBase):
                              value=[row, col, child.render()])
         self.make_callback('widget-added', child)
 
+    def remove(self, child, delete=False):
+        # need to delete the child from self.tbl
+        children = list(self.tbl.values())
+        keys = list(self.tbl.keys())
+        idx = children.index(child)
+        key = keys[idx]
+        del self.tbl[key]
+
+        super().remove(child, delete=delete)
+
     def get_widget_at_cell(self, row, col):
         return self.tbl[(row, col)]
 