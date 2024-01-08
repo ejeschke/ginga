f39c74fdb4c211937b84ae917156fb781559a57c
Eric Jeschke <eric@naoj.org>
2024-01-08 11:45:36 -1000

    Remove stubs for relocated IO modules

--- ginga/util/io_fits.py
+++
@@ -1,6 +0,0 @@
-import warnings
-warnings.warn("this module has moved; "
-              "change your import to ginga.util.io.io_fits",
-              DeprecationWarning)
-
-from ginga.util.io.io_fits import *   # noqa
--- ginga/util/io_rgb.py
+++
@@ -1,6 +0,0 @@
-import warnings
-warnings.warn("this module has moved; "
-              "change your import to ginga.util.io.io_rgb",
-              DeprecationWarning)
-
-from ginga.util.io.io_rgb import *   # noqa
