8523c71f0892544fa2ac7b368da240b4e4f94dbf
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-02-21 17:18:20 -0500

    MNT: Remove astropy test runner

--- ginga/__init__.py
+++ ginga/__init__.py
@@ -1,10 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.txt
 """See LONG_DESC.txt"""
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *  # noqa
-# ----------------------------------------------------------------------------
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
 
 # END
